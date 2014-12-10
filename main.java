51e034bec99376bbc55e3ac54c9e434a5cf5e45e
Sodant <micksoudant@gmail.com>
2014-12-10 14:23:38 +0100

    Change-Id: I70e2cf22fa1a45286059480871a7469d58411e24

---
+++ main.java
@@ -0,0 +1,11 @@
+import org.checkerframework.checker.initialization.qual.Initialized;
+import org.checkerframework.checker.nullness.qual.NonNull;
+
+public class main {
+	
+	public static void main(String[] args) 
+	{
+
+	}
+
+}
