2483b397aa8850d073ccb70ab225b2e93bdb8f79
Sodant <micksoudant@gmail.com>
2014-12-15 12:14:28 +0100

    Updated human and humantest
    added more attributes to human and refactored the test.

--- src/humanresourcemanagement/Human.java
+++ src/humanresourcemanagement/Human.java
@@ -4,20 +4,47 @@ import java.time.LocalDateTime;
 
 public class Human {
 	
-	enum Gender {
+	public enum Gender {
 		MALE, FEMALE
 	}
 	
 	private int age;
 	private String firstName, lastName;
-	private Gender gender;
-	
+	private Gender gender;	
 	private LocalDateTime birthDate;
-		
-		
-	public void Render(/** **/){
-		//presentation?
-	}
 	
+	public Human (String firstName, String lastName, int age, Gender gender){
+		this.firstName = firstName;
+		this.lastName = lastName;
+		this.age = age;
+	}
+		
+	public void Render(/** **/){		
+		//--		
+	}
+
+	public int getAge() {
+		return age;
+	}
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	public LocalDateTime getBirthDate() {
+		return birthDate;
+	}
+
+	public void setBirthDate(LocalDateTime birthDate) {
+		this.birthDate = birthDate;
+	}
+
+	private Gender getGender() {
+		return gender;
+	}
 
 }
--- src/humanresourcemanagement/unittests/HumanTest.java
+++ src/humanresourcemanagement/unittests/HumanTest.java
@@ -1,23 +1,27 @@
 package humanresourcemanagement.unittests;
 
 import static org.junit.Assert.*;
+
 import org.junit.Before;
 import org.junit.Test;
+
 import humanresourcemanagement.Human;
+import humanresourcemanagement.Human.Gender;
 
 public class HumanTest {
 	
 	Human testSubject;
+	
 	@Before
 	public void initialize()
 	{
-		testSubject = new Human();
-		
+		testSubject = new Human("Mick", "Soudant", 23, Gender.MALE);		
 	}
 	
 	@Test
-	public void getNameTest() 
+	public void initializeTest() 
 	{	
 		assertEquals(true, true);
-	}	
+	}
+	
 }
